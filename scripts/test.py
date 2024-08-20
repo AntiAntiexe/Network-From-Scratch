8e4a9e6b5e30e0078b7e02bffe1464e062194357
AntiAntiexe <mrbricken@gmail.com>
2024-08-20 19:56:11 +1000

    backpropagation testing

--- scripts/test.py
+++ scripts/test.py
@@ -1,4 +1,4 @@
-'''import math
+"""import math
 
 softmax_output = [0.7, 0.1, 0.2]
 target_output = [1, 0, 0]
@@ -7,10 +7,11 @@ loss = -(math.log(softmax_output[0])*target_output[0] +
          math.log(softmax_output[1])*target_output[1] +
          math.log(softmax_output[2])*target_output[2])
 
-print(loss)'''
+print(loss)"""
 
 import numpy as np
-'''
+
+"""
 softmax_output = np.array([[0.7, 0.1, 0.2],
                            [0.1, 0.5, 0.4],
                            [0.02, 0.9, 0.08]])
@@ -22,9 +23,9 @@ neg_log = -np.log(softmax_output[range(len(softmax_output)), class_targets])
 average_loss = np.mean(neg_log)
 
 print(average_loss)
-'''
+"""
 
-'''samples = 3
+"""samples = 3
 y_pred_clipped = np.array([[0.7, 0.1, 0.2],
                            [0.1, 0.5, 0.4],
                            [0.02, 0.9, 0.08]])
@@ -33,9 +34,9 @@ y_true = [0, 1, 1]
 if len(y_true.shape) == 1:
     correct_confidences = y_pred_clipped[range(samples), y_true]
 
-print(correct_confidences)'''
+print(correct_confidences)"""
 
-'''
+"""
 softmax_output = np.array([[0.7, 0.1, 0.2],
                            [0.1, 0.5, 0.4],
                            [0.02, 0.9, 0.08]])
@@ -46,16 +47,52 @@ predictions = np.argmax(softmax_output, axis=1)
 accuracy = np.mean(predictions == class_targets)
 
 print(accuracy)
-'''
+"""
+
+x = [1.0, -2.0, 3.0]  # input values
+w = [-3.0, -1.0, 2.0]  # Weights
+b = 1.0  # Bias
+
+xw0 = x[0] * w[0]
+xw1 = x[1] * w[1]
+xw2 = x[2] * w[2]
+
+z = xw0 + xw1 + xw2 + b
+
+y = max(z, 0)
+
+dvalue = 1.0
+
+drelu_dz = dvalue * (1.0 if z > 0 else 0.0)
+print(drelu_dz)
+
+dsum_dxw0 = 1.0
+dsum_dxw1 = 1.0
+dsum_dxw2 = 1.0
+dsum_db = 1.0
+
+drelu_dxw0 = drelu_dz * dsum_dxw0
+drelu_dxw1 = drelu_dz * dsum_dxw0
+drelu_dxw2 = drelu_dz * dsum_dxw0
+
+drelu_db = drelu_dz * dsum_db
+print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
+
+dmul_dx0 = w[0]
+dmul_dx1 = w[1]
+dmul_dx2 = w[2]
+
+dmul_dw0 = x[0]
+dmul_dw1 = x[1]
+dmul_dw2 = x[2]
 
 
-import matplotlib.pyplot as plt
-import nnfs
-from nnfs.datasets import spiral_data
+drelu_dx0 = drelu_dxw0 * dmul_dx0
+drelu_dx1 = drelu_dxw1 * dmul_dx1
+drelu_dx2 = drelu_dxw2 * dmul_dx2
 
-nnfs.init()
+drelu_dw0 = drelu_dxw0 * dmul_dw0
+drelu_dw1 = drelu_dxw1 * dmul_dw1
+drelu_dw2 = drelu_dxw2 * dmul_dw2
 
-X, y = spiral_data(samples=100, classes=2)
-
-plt.scatter(X  [:, 0], X[:, 1], c=y, s=40, cmap='brg')
-plt.show()
+print(drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
