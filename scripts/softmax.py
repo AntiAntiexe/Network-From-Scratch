4d73efafb9b9a3e65a195fe3e624a952e8fb79a1
AntiAntiexe <mrbricken@gmail.com>
2024-09-22 19:07:13 +1000

    Applied the loss function and can see accuracy

--- scripts/softmax.py
+++ scripts/softmax.py
@@ -1,23 +1,8 @@
-import numpy as np
-# Passed in gradient from the next layer
-# for the purpose of this example we're going to use
-# a vector of 1s
-dvalues = np.array([[ 1. , 1. , 1. ]])
-# We have 3 sets of weights - one set for each neuron
-# we have 4 inputs, thus 4 weights
-# recall that we keep weights transposed
-weights = np.array([[ 0.2 , 0.8 ,
-- 0.5 , 1 ],
-[ 0.5 ,
-- 0.91 , 0.26 ,
-- 0.5 ],
-[ - 0.26 ,
-- 0.27 , 0.17 , 0.87 ]]).T
-# sum weights of given input
-# and multiply by the passed in gradient for this neuron
-dx0 = sum (weights[ 0 ]) * dvalues[ 0 ]
-dx1 = sum (weights[ 1 ]) * dvalues[ 0 ]
-dx2 = sum (weights[ 2 ]) * dvalues[ 0 ]
-dx3 = sum (weights[ 3 ]) * dvalues[ 0 ]
-dinputs = np.array([dx0, dx1, dx2, dx3])
-print (dinputs)
+def gradient_descent(iterations, learning_rate, init):
+
+    for i in range(iterations):
+        d = 2 * init
+        init = init - learning_rate * d
+    return round(init, 5)
+
+print(gradient_descent(10, 0.01, 2))
