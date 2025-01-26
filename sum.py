56bd1087589d71f7a46852af89f4982bb2956904
AntiAntiexe <mrbricken@gmail.com>
2025-01-26 19:31:32 +1100

    part 4

---
+++ sum.py
@@ -0,0 +1,24 @@
+inputs = [1, 2, 3, 2.5]
+weights = [[0.2, 0.8, -0.5, 1],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+biases = [2, 3, 0.5]
+
+# Output of current layer
+layer_outputs = []
+# for each neuron weight and neuron bias in weights and biases respectively, and the indexes will be the same
+for neuron_weights, neuron_bias in zip(weights, biases):
+    # Zeroed output of given neuron
+    neuron_output = 0
+
+    for n_input, weight in zip(inputs, neuron_weights):
+        # sum the weights times the inputs
+        neuron_output += weight * n_input
+
+    # Add bias
+    neuron_output += neuron_bias
+
+    #We then put the neuron out puts to a list of this layers outputs, so if there was a neuron after this one it can use these as the input.
+    layer_outputs.append(neuron_output)
+
+print(layer_outputs)
